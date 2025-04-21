7591041a78e3fa46677affaadcaaa9dbf6c92140
Olaitan Majekodunmi <majekolaitan@gmail.com>
2025-04-21 08:17:22 +0100

    changed egw channel

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ USER_HOME_DIR = os.path.expanduser('~')
 ROOT_DIR = os.path.join(USER_HOME_DIR, 'Videos', 'Lesson Study')
 
 # Define channel IDs and query formats
+# Channel E.G. White Audio 'id': 'UCPS3A-60tKmKTCKWZMT9upA'
 CHANNEL_IDS = {
     'hopess': {'id': 'UCm34NbuHzE9t9hHutOxwIOA', 'query_format': 'Lesson {lesson_number}', 'first_result': False, 'confirm_download': True},
     'sabbathschoollikeplus': {'id': 'UCkO6iQI4HBYFolkrbmQWDHw', 'query_format': '{lesson_title} Lesson {lesson_number} Q{quarter} {year}', 'first_result': True, 'confirm_download': False},
@@ -12,5 +13,5 @@ CHANNEL_IDS = {
     '3abn': {'id': 'UCw_AthKfwqB3XYpboTFZFmg', 'query_format': '{lesson_title} | Sabbath School Panel by 3ABN - Lesson {lesson_number} Q{quarter} {year}', 'first_result': True, 'confirm_download': False},
     'itiswritten': {'id': 'UCtWyoUrGPAkZgnp2486Ir4w', 'query_format': 'Sabbath School - {year} Q{quarter} Lesson {lesson_number}: {lesson_title}', 'first_result': True, 'confirm_download': False},
     'HopeLives365': {'id': 'UCOuDMda3jxj-g_iI1P2d2zw', 'query_format': 'Sabbath School with Mark Finley | Lesson {lesson_number} — Q{quarter} – {year}', 'first_result': True, 'confirm_download': False},
-    'egwhiteaudio': {'id': 'UCPS3A-60tKmKTCKWZMT9upA', 'query_format': '{year} Q{quarter} Lesson {lesson_number} – EGW Notes – {lesson_title}', 'first_result': True, 'confirm_download': False},
+    'egwhiteaudio': {'id': 'UCvJRu-jirSkv6yuxakirENg', 'query_format': '{year} Q{quarter} Lesson {lesson_number} – EGW Notes – {lesson_title}', 'first_result': True, 'confirm_download': False},
 }
